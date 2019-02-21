410bc8d91d48c839f780a897fd093d81baf7b930
Eric Capone <ericcapone@Erics-MacBook-Pro.local>
2019-02-21 14:08:42 -0500

    First commit

---
+++ app/data/friends.js
@@ -0,0 +1,149 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Abbi',
+		profilePic: 'https://pixel.nymag.com/imgs/daily/vulture/2018/07/12/splitsider/abbi-jacobson-931133494.w330.h330.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Fry',
+		profilePic: 'http://www.cartoonswallpapers.net/wallpapers/futurama-fry-full-hd-background-macbook.png',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Barack',
+		profilePic: 'https://upload.wikimedia.org/wikipedia/commons/8/8d/President_Barack_Obama.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Vanilla Ice',
+		profilePic: 'https://assets.dmagstatic.com/wp-content/uploads/2015/04/vanilla-ice.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Billy',
+		profilePic: 'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimages.hellogiggles.com%2Fuploads%2F2017%2F03%2F15234107%2Fadam-sandler-in-billy-madison.jpg&w=450&c=sc&poi=face&q=85',
+		scores: [
+			'1',
+			'3',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Beyonce Knowles',
+		profilePic: 'http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Gritty',
+		profilePic: 'https://cnet4.cbsistatic.com/img/zNB5S30mUqvuWUfzDg1D8HaIIpo=/1600x900/2018/12/04/61f8b458-128f-4da9-b956-bd80527286e0/gritty-closeup.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'4',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Kermit the Frog',
+		profilePic: 'http://i.dailymail.co.uk/i/pix/2015/09/02/08/2BE5406100000578-3218820-image-m-25_1441179775589.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Rick',
+		profilePic: 'https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest?cb=20160923150728',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
